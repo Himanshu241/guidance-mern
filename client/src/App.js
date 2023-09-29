7aec95606eee1fb7a62be4d25ec1c4ce44eb2257
himanshu <himanshuchaudhary429@gmail.com>
2023-09-29 23:13:11 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+
+import SignupForm from './scenes/loginPage.jsx';
+
+function App() {
+  return (
+    <div><SignupForm /></div>
+  );
+}
+
+export default App;
---
+++ client/src/scenes/loginPage.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const SignupForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can handle form submission logic here
+    console.log('Submitted Data:', formData);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h2>Signup </h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">First Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Last Name</label>
+          <input
+            type="text"
+            className="form-control"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Signup
+        </button>
+        
+      </form>
+    </div>
+  );
+};
+
+export default SignupForm;
