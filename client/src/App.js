950612ec3437c473a28fcea9aebce6611db76a61
himanshu <himanshuchaudhary429@gmail.com>
2023-10-03 14:37:49 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 
 import SignupForm from './scenes/loginPage.jsx';
-
+import Intro from './scenes/intro.jsx'
+import './App.css'
 function App() {
   return (
-    <div><SignupForm /></div>
+    <>
+    <Intro />
+    <SignupForm />
+    </>
   );
 }
 
---
+++ client/src/scenes/intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const intro = () => {
+  return (
+    <div>
+        
+        <h1 className='title' >MargDarshan</h1>
+
+    </div>
+  )
+}
+
+export default intro
