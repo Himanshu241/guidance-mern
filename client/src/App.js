df5a934f6255aafc42a000f98a76298b897ba69e
himanshu <himanshuchaudhary429@gmail.com>
2023-10-04 23:15:03 +0530

    buttons added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-
+import {BrowserRouter, Navigate, Routes, Route} from 'react-router-dom';
 import SignupForm from './scenes/loginPage.jsx';
 import Intro from './scenes/intro.jsx'
 import './App.css'
 function App() {
   return (
-    <>
-    <Intro />
-    <SignupForm />
-    </>
+    <div className='app' >
+      <BrowserRouter>
+        
+        <Routes>
+            <Route path="/" element={<Intro/>}></Route>
+            <Route path="/signUp" element={<SignupForm/>}></Route>
+        </Routes>
+       
+      </BrowserRouter> 
+    </div>
   );
 }
 
--- client/src/scenes/intro.jsx
+++ client/src/scenes/intro.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import CustomButton from '../components/button'
 
 
 const intro = () => {
+
+  const handleSignIn=()=>{
+      alert('sign in')
+  }
+  const handleSignUp = ()=>{
+    alert('sign up')
+  }
+
   return (
     <div>
         
         <h1 className='title' >MargDarshan</h1>
+        <CustomButton color="#845ef7" onClick={handleSignUp} text="sign-up"></CustomButton>
+        <CustomButton color="#845ef7" onClick={handleSignIn} text="sign-in"></CustomButton>
 
     </div>
   )
