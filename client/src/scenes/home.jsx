5aeb62b665c272062839f9ea86f39081bd7b63a7
himanshu <himanshuchaudhary429@gmail.com>
2023-10-28 17:09:18 +0530

    redux for auth

--- client/src/scenes/home.jsx
+++ client/src/scenes/home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Foot from './Foot';
+import { useState } from 'react';
+import { UseSelector, useSelector } from 'react-redux/es/hooks/useSelector';
 function Home() {
+  const token = useSelector((state)=>state.token); 
+  const userName = useSelector((state)=>state.user.name);
+   
   return (
     <>
-    <div>home</div>
+    <div className='text-white'>{token}</div>
+    <div className='text-white'>{userName}</div>
     <Foot/>
     </>
   )
